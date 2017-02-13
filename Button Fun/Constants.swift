ea3065bc605293a3fcef141fa36bcadd1a726504
Noemi Quezada <noemi01@csu.fullerton.edu>
2017-02-12 22:48:00 -0800

    Created a Block class, which is the UI Model, extending the UIColor class to hold a static function that returns a random color, created a grid utilizing nested stackviews, the Blocks are tap enabled

---
+++ Button Fun/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Button Fun
+//
+//  Created by Noemi Quezada on 2/12/17.
+//  Copyright © 2017 Noemi Quezada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    
+    // MARK: Block Constant Properties
+    static let BLOCK_HEIGHT = CGFloat(40);
+    static let BLOCK_WIDTH = CGFloat(40);
+}
--- Button Fun/ViewController.swift
+++ Button Fun/ViewController.swift
@@ -9,10 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let data:ColorSource = ColorSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillLayoutSubviews() {
+
+        // Compute grid constraints
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        let columns = Int(width / Constants.BLOCK_WIDTH)
+        let rows = Int(height / Constants.BLOCK_HEIGHT)
+        
+        // Create Grid
+        var columnStackViews = [UIStackView]()
+        
+        for _ in 0...rows-1 {
+            var blocks = [Block]()
+            for _ in 0...columns-1 {
+                // Create a view
+                let view = Block()
+            
+                blocks.append(view);
+            }
+            
+            // Create Columns
+            let stackView = UIStackView(arrangedSubviews: blocks)
+            stackView.axis = .horizontal
+            stackView.distribution = .fillEqually
+            stackView.alignment = .center
+            stackView.spacing = 0
+            
+            columnStackViews.append(stackView)
+        }
+        
+        // Create Row
+        let stackView = UIStackView(arrangedSubviews: columnStackViews)
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        // Add Constraints to the Stack View
+        let viewsDictionary = ["stackView":stackView]
+        let stackView_H = NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[stackView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: viewsDictionary)
+        let stackView_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[stackView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: viewsDictionary)
+        view.addConstraints(stackView_H)
+        view.addConstraints(stackView_V)
+        
     }
 
     override func didReceiveMemoryWarning() {
