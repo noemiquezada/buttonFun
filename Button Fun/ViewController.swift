d9b7a3f173249d2cf3d06973d67638513173c606
Noemi Quezada <noemi01@csu.fullerton.edu>
2017-02-12 17:31:27 -0800

    Initial Commit

---
+++ Button Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Button Fun
+//
+//  Created by Noemi Quezada on 2/12/17.
+//  Copyright © 2017 Noemi Quezada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
